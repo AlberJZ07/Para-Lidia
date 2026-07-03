a1d610a9723c40161a98d872d34c122105e57b20
AlberJZ07 <alberzurita7@gmail.com>
2026-07-03 15:28:51 -0500

    Update loader display and welcome alert
    Refactor welcome message display and loader behavior.

--- script.js
+++ script.js
@@ -1,20 +1,15 @@
-document.getElementById("entrar").addEventListener("click", function () {
+setTimeout(()=>{
 
-    document.body.style.transition = "1s";
-    document.body.style.background = "linear-gradient(135deg,#ff4d6d,#ff758f)";
+document.querySelector(".loader").style.display="none";
 
-    setTimeout(() => {
-        document.querySelector(".loader").innerHTML = `
-            <h1>Bienvenida mi amor ❤️</h1>
+document.querySelector(".contenido").classList.remove("oculto");
 
-            <h2 style="margin-top:20px;">Lidia Ayne Garcia Ruiz</h2>
+document.querySelector(".contenido").style.opacity="1";
 
-            <p style="max-width:700px;margin:30px auto;font-size:22px;line-height:1.8;">
-            Este pequeño lugar fue creado únicamente para ti.
-            Aquí guardaré nuestros recuerdos, nuestras risas y cada momento que hemos vivido juntos.
-            Espero que cada vez que entres aquí recuerdes cuánto te amo. ❤️
-            </p>
-        `;
-    },1000);
+},3000);
 
-});
+document.getElementById("entrar").onclick=function(){
+
+alert("Bienvenida mi amor ❤️");
+
+}

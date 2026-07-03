6b691e4e3bb94aab1babce2f84f147557890f038
AlberJZ07 <alberzurita7@gmail.com>
2026-07-03 15:33:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -13,3 +13,24 @@ document.getElementById("entrar").onclick=function(){
 alert("Bienvenida mi amor ❤️");
 
 }
+function crearCorazon(){
+
+const corazon=document.createElement("div");
+
+corazon.className="corazon";
+
+corazon.innerHTML="❤️";
+
+corazon.style.left=Math.random()*100+"vw";
+
+corazon.style.fontSize=(20+Math.random()*25)+"px";
+
+document.body.appendChild(corazon);
+
+setTimeout(()=>{
+corazon.remove();
+},8000);
+
+}
+
+setInterval(crearCorazon,500);
